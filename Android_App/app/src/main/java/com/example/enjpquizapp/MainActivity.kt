f0665ebbee9af4512ad8890a29e2d3912dac7e1c
quietThinker2 <127311105+quietThinker2@users.noreply.github.com>
2023-05-24 07:10:34 -0600

    Start Activity Nearly Done
    -Database is connected
    -Able to display database to the app
    -Start/main Activity has a ideal Layout
    -Able to exit

--- Android_App/app/src/main/java/com/example/enjpquizapp/MainActivity.kt
+++ Android_App/app/src/main/java/com/example/enjpquizapp/MainActivity.kt
@@ -2,10 +2,79 @@ package com.example.enjpquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnExit: Button = findViewById(R.id.btnExit)
+        btnExit.setOnClickListener{
+            //finish()
+            exitProcess(1)
+        }
+
+        val dbHelper = SQLiteHelper(this)
+        val data = dbHelper.getAData()
+
+        createButtonsFromDataBase(data)
+
+        val textView: TextView = findViewById(R.id.textViewTest)
+        textView.text = data
+
+        dbHelper.close()
+
+    }
+
+    fun createButtonsFromDataBase(data: String) {
+        val buttonContainer: LinearLayout = findViewById(R.id.buttonContainer)
+
+        val values = data.split("\n") // Split the data into individual values
+        for (value in values) {
+
+            //remove last empty button
+            if (value.isEmpty()){
+                break
+            }
+            val button = Button(this)
+            button.text = value
+
+            // Set any other properties or listeners for the button as needed
+
+            //set button id
+            val generatedId = View.generateViewId()
+            button.id = generatedId
+
+
+            //set button text size
+            val textSizePx = resources.getDimensionPixelSize(R.dimen.button_text_size)
+            button.setTextSize(TypedValue.COMPLEX_UNIT_PX,textSizePx.toFloat())
+
+
+            //action on clicked
+            button.setOnClickListener{
+                val clickedButtonId = it.id
+                println("Clicked Button ID: $clickedButtonId")
+            }
+
+
+            // Add the buttons to the button container
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            layoutParams.setMargins(0,0,0,16)
+
+
+            buttonContainer.addView(button, layoutParams)
+        }
     }
 }
---
+++ Android_App/app/src/main/java/com/example/enjpquizapp/SQLiteHelper.kt
@@ -0,0 +1,80 @@
+package com.example.enjpquizapp
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+
+class SQLiteHelper(private val context: Context) :
+SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+
+    companion object {
+        private const val DATABASE_NAME = "EN-JB-DB.db"
+        private const val DATABASE_VERSION = 1
+    }
+
+    private val dbPath: String = context.getDatabasePath(DATABASE_NAME).path
+
+    init {
+        copyDatabaseFromAssets(context)
+    }
+
+    //load database
+    private fun copyDatabaseFromAssets(context: Context) {
+        val dbFile = File(dbPath)
+        if (!dbFile.exists()) {
+            try {
+                val inputStream = context.assets.open(DATABASE_NAME)
+                val outputStream = FileOutputStream(dbPath)
+
+                val buffer = ByteArray(1024)
+                var length: Int
+                while (inputStream.read(buffer).also { length = it } > 0) {
+                    outputStream.write(buffer, 0, length)
+                }
+
+                outputStream.flush()
+                outputStream.close()
+                inputStream.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        // Database creation logic if needed
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // Database upgrade logic if needed
+    }
+
+    fun getAData(): String {
+        val db = readableDatabase
+        val projection = arrayOf("segmentNumber", "segmentName")
+        val cursor = db.query("DATA", projection, null, null, null, null, null)
+
+        val stringBuilder = StringBuilder()
+        while (cursor.moveToNext()) {
+            val segmentNumber = cursor.getString(cursor.getColumnIndexOrThrow("segmentNumber"))
+            val segmentName = cursor.getString(cursor.getColumnIndexOrThrow("segmentName"))
+
+            val concatValue = "$segmentNumber - $segmentName"
+            if (segmentNumber.isNotEmpty()){
+                if (!stringBuilder.contains(concatValue)){
+
+                    stringBuilder.append(concatValue).append("\n")
+                }
+            }
+
+
+        }
+
+        cursor.close()
+        return stringBuilder.toString()
+    }
+}
